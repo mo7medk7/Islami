beace2ace5deebb65025ef7237fe96287536b57b
mo7medk7 <m>
2023-09-17 18:34:47 +0200

    Finished home screen .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/Home/HomeScreen.dart';
+import 'package:islami_app/myTheme.dart';
 
 void main (){
   runApp(myApp());
@@ -12,12 +13,11 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       initialRoute: HomeScreen.routeName,
-
       routes: {
-        HomeScreen.routeName : (context) => HomeScreen(),
+        HomeScreen.routeName: (context) => HomeScreen(),
       },
+      theme: myTheme.LightTheme,
     );
   }
 }
